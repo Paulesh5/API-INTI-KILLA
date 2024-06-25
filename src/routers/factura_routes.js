350e3eaecaa0636ad0968dfd0880fc1b3b917f2e
Paul Hidalgo <paulesh5@gmail.com>
2024-06-24 22:39:33 -0500

    factura_routes

--- src/routers/factura_routes.js
+++ src/routers/factura_routes.js
@@ -1,6 +1,6 @@
 import {Router} from 'express'
 const router = Router()
-import { generateInvoiceXml } from '../controllers/factura_controller.js';
+import { generateInvoiceXml, recepcionSRI, autorizacionSRI } from '../controllers/factura_controller.js';
 import { detalleFactura, listarFacturas } from '../controllers/facturaListar_controller.js';
 import { generatePdf } from '../controllers/facturaPDF_controller.js';
 import verificarAutenticacion from "../middlewares/autenticacion.js";
@@ -10,6 +10,8 @@ import { validacionFactura } from '../middlewares/validacionFactura.js';
 router.get('/facturas', verificarAutenticacion, listarFacturas)
 router.get('/factura/:id', verificarAutenticacion, detalleFactura)
 router.post('/factura/generate-invoice', verificarAutenticacion, validacionFactura, generateInvoiceXml)
+router.post('/factura/recibir', verificarAutenticacion, recepcionSRI)
+router.post('/factura/autorizacion', verificarAutenticacion, autorizacionSRI)
 router.get('/factura/generate-pdf/:id', verificarAutenticacion, generatePdf)
 
 
